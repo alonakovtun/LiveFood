e2ae1755271277662f7d8fa23a2e11dfd6be542b
Alona Kovtun <alonakovtun55@gmail.com>
2021-10-22 16:42:52 +0100

    comments

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravelista\Comments\Commentable;
 
 class Recipe extends Model
 {
     use HasFactory;
+    use Commentable;
 
     protected $primaryKey = 'id';
     public $incrementing = false;
