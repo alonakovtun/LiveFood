cb1e8b31b5d389a298365192bb4883cea014bc68
Alona Kovtun <alonakovtun55@gmail.com>
2021-12-07 11:32:19 +0100

    policy & terms

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,12 +1,80 @@
 <x-guest-layout>
     <div class="pt-4 bg-gray-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
-            <div>
-                <x-jet-authentication-card-logo />
-            </div>
-
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $terms !!}
+            <div class="w-full sm:max-w-2xl mt-6 mb-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
+                <h1>Terms of Service</h1>
+                <ol>
+                    <li class="text-2xl">
+                        <b>Terms</b>
+                        <p class="text-base">By accessing this Website, accessible from <u>Website.com</u>, you are agreeing to be bound by 
+                            these Website Terms and Conditions of Use and agree that you are responsible for the agreement 
+                            with any applicable local laws. If you disagree with any of these terms, you are prohibited from 
+                            accessing this site. The materials contained in this Website are protected by copyright and 
+                            trade mark law.</p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Use License</b>
+                        <p class="text-base">Permission is granted to temporarily download one copy of the materials
+                            on <u>Company Name's</u> Website for personal, non-commercial transitory viewing only. 
+                            This is the grant of a license, not a transfer of title, and under this license you may not:
+                        </p>
+                        <ul>
+                            <li class="text-base">modify or copy the materials;</li>
+                            <li class="text-base">use the materials for any commercial purpose or for any public display;</li>
+                            <li class="text-base">attempt to reverse engineer any software contained on <u>Company Name's</u> Website;</li>
+                            <li class="text-base">remove any copyright or other proprietary notations from the materials; or</li>
+                            <li class="text-base">transferring the materials to another person or "mirror" the materials on any other server.</li>
+                        </ul>
+                        <p class="text-base">This will let <u>Company Name</u> to terminate upon violations of any of these restrictions. 
+                            Upon termination, your viewing right will also be terminated and you should destroy any downloaded 
+                            materials in your possession whether it is printed or electronic format.
+                        </p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Disclaimer</b>
+                        <p class="text-base">All the materials on <u>Company Name's</u> Website are provided “as is”. 
+                            <u>Company Name's</u> makes no warranties, may it be expressed or implied, therefore negates all other warranties.
+                            Furthermore, <u>Company Name's</u> does not make any representations concerning the accuracy or reliability of 
+                            the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.
+                        </p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Limitations</b>
+                        <p class="text-base"><u>Company Name's</u> or its suppliers will not be hold accountable for any damages that will arise 
+                            with the use or inability to use the materials on <u>Company Name's</u> Website, even if <u>Company Name's</u> or an authorize 
+                            representative of this Website has been notified, orally or written, of the possibility of such damage. 
+                            Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, 
+                            these limitations may not apply to you.
+                        </p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Revisions and Errata</b>
+                        <p class="text-base">The materials appearing on <u>Company Name's</u> Website may include technical, typographical, 
+                            or photographic errors. <u>Company Name's</u> will not promise that any of the materials in this Website are accurate, 
+                            complete, or current. <u>Company Name's</u> may change the materials contained on its Website at any time without notice.
+                            <u>Company Name's</u> does not make any commitment to update the materials.
+                        </p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Links</b>
+                        <p class="text-base"><u>Company Name's</u> has not reviewed all of the sites linked to its Website and is not responsible 
+                            for the contents of any such linked site. The presence of any link does not imply endorsement by <u>Company Name's</u> of the site. 
+                            The use of any linked website is at the user's own risk.
+                        </p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Site Terms of Use Modifications</b>
+                        <p class="text-base"><u>Company Name's</u> may revise these Terms of Use for its Website at any time without prior notice. 
+                            By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.
+                        </p>
+                    </li>
+                    <li class="text-2xl">
+                        <b>Governing Law</b>
+                        <p class="text-base">Any claim related to <u>Company Name's</u> Website shall be governed by the laws of Country without 
+                            regards to its conflict of law provisions.
+                        </p>
+                    </li>
+                </ol>
             </div>
         </div>
     </div>
